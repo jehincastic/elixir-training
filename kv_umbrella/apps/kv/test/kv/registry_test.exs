e32d3cc529445deb72c8df3fd7d1247d8840007b
jehincastic <jehincastic@gmail.com>
2022-09-11 22:19:04 +0530

    Added More

---
+++ kv_umbrella/apps/kv/test/kv/registry_test.exs
@@ -0,0 +1,37 @@
+defmodule Kv.RegistryTest do
+  use ExUnit.Case, async: true
+
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
+  end
+
+  test "spawns buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+
+    # assert KV.Registry.lookup(registry, "shopping") == :error
+    catch_exit KV.Bucket.put(bucket, "shopping", 3)
+  end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
+
+    catch_exit KV.Bucket.put(bucket, "shopping", 3)
+  end
+end
